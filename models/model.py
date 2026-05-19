f7853e467d1ee33ef011a3cd63a8165f7406ebe1
Sweden <sweden.agunenye1@gmail.com>
2026-05-19 15:29:10 -0700

    adapted pretrained model to live detection, only classification

--- models/model.py
+++ models/model.py
@@ -8,7 +8,7 @@ from IPython.display import Image, display
 path = kagglehub.dataset_download("muhriddinmuxiddinov/fruits-and-vegetables-dataset")
 
 # Load a pretrained YOLOv8m model (model weights)
-model = YOLO('models/yolo_fruits_and_vegetables_v1.pt')
+model = YOLO("models/yolo_fruits_and_vegetables_v1.pt")
 
 # Set confidence and iou threshold
 conf = 0.6
@@ -16,7 +16,9 @@ iou = 0.1
 # show_results = False
 
 # Run batched inference on a list of images
-results = model.predict(source=0,show=True,conf=0.6,iou=0.1,stream=True)  # return a list of Results objects
+results = model.predict(
+    source=0, show=True, conf=0.6, iou=0.1, stream=True, device="mps", half=True
+)  # return a list of Results objects
 
 # if show_results:
 #     for result in results:
@@ -28,11 +30,7 @@ results = model.predict(source=0,show=True,conf=0.6,iou=0.1,stream=True)  # retu
 
 # Save results and show detections
 for r in results:
-    r.show()
+    # r.show()
+    pass
     # # save locally
     # result.save(filename=f'Example_Results/{os.path.basename(result.path)}.png')
-
-# Show images
-
-# for i in range(2):
-#     display(Image(f'Example_Results/{os.path.basename(images[i])}.png', width=200))
--- models/test.py
+++
@@ -1,4 +0,0 @@
-import kagglehub
-
-path = kagglehub.dataset_download("henningheyen/lvis-fruits-and-vegetables-dataset")
-print(path)
