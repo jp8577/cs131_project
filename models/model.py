5f3b419dc53e0cea5e053a108bb19aca11c8c807
Sweden <sweden.agunenye1@gmail.com>
2026-05-28 18:07:52 -0700

    updated file path of weights

--- models/model.py
+++ models/model.py
@@ -3,14 +3,15 @@ from ultralytics import YOLO
 
 # Inference script
 
-MODEL_PATH = Path(__file__).parent / "runs/detect/train-4/weights/best.pt"
+MODEL_PATH = Path(__file__).parents[1] / "runs/detect/train-9/weights/best.pt"
 
 model = YOLO(MODEL_PATH)
 
 results = model.predict(
     source=0,  # Logitech Brio 101 on /dev/video0
     show=True,
-    device=0,
+    device=0,  # GPU found on device can be enabled with CUDA on Orin Nano or
+    # device="mps" for optimization on Apple Silicon
     half=True,  # Model quantized to FP16 precision
     conf=0.5,  # Confidence level
     iou=0.4,  # Intersection on union threshold
